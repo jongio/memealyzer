52fceb6e61056ae60a0677dd6c88acb8f1b1c70d
Jon Gallant <jon@jongallant.com>
2020-06-30 13:45:55 -0700

    Improve the python queue service

--- src/python/services/queue-service/queue_service.py
+++ src/python/services/queue-service/queue_service.py
@@ -1,38 +1,63 @@
-from azure.identity import AzureCliCredential
-from azure.storage.queue import QueueServiceClient
-from azure.ai.formrecognizer import FormRecognizerClient
-from azure.ai.textanalytics import TextAnalyticsClient
-from azure.cosmos import CosmosClient
-
 import requests
 import os
 import time
 import json
 
+from azure.identity import (
+    AzureCliCredential,
+    ChainedTokenCredential,
+    ManagedIdentityCredential,
+    EnvironmentCredential,
+)
+from azure.storage.queue import QueueServiceClient
+from azure.ai.formrecognizer import FormRecognizerClient
+from azure.ai.textanalytics import TextAnalyticsClient
+from azure.cosmos import CosmosClient
+
+from smart_getenv import getenv
 from dotmap import DotMap
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
 
-load_dotenv("../../.env")
+load_dotenv(find_dotenv())
 
-credential = AzureCliCredential()
+credential = ChainedTokenCredential(
+    AzureCliCredential(), EnvironmentCredential(), ManagedIdentityCredential()
+)
 
 queue_service_client = QueueServiceClient(
-    account_url=os.getenv("AZURE_STORAGE_QUEUE_ENDPOINT"), credential=credential
+    account_url=getenv("AZURE_STORAGE_QUEUE_ENDPOINT"), credential=credential
 )
+
 queue_client = queue_service_client.get_queue_client(
-    queue=os.getenv("AZURE_STORAGE_QUEUE_NAME")
+    queue=getenv("AZURE_STORAGE_QUEUE_NAME", default="messages")
 )
 
-fr_client = FormRecognizerClient(endpoint=os.getenv('AZURE_FORM_RECOGNIZER_ENDPOINT'), credential=credential)
-ta_client = TextAnalyticsClient(endpoint=os.getenv('AZURE_TEXT_ANALYTICS_ENDPOINT'), credential=credential)
-cosmos_client = CosmosClient(url=os.getenv('AZURE_COSMOS_ENDPOINT'), credential=os.getenv('AZURE_COSMOS_KEY'))
-cosmos_database_client = cosmos_client.get_database_client(os.getenv('AZURE_COSMOS_DB'))
-cosmos_container_client = cosmos_database_client.get_container_client(os.getenv('AZURE_COSMOS_CONTAINER'))
+fr_client = FormRecognizerClient(
+    endpoint=getenv("AZURE_FORM_RECOGNIZER_ENDPOINT"), credential=credential
+)
 
-while(True):
-    
+ta_client = TextAnalyticsClient(
+    endpoint=getenv("AZURE_TEXT_ANALYTICS_ENDPOINT"), credential=credential
+)
+
+cosmos_client = CosmosClient(
+    url=getenv("AZURE_COSMOS_ENDPOINT"), credential=getenv("AZURE_COSMOS_KEY")
+)
+
+cosmos_database_client = cosmos_client.get_database_client(
+    getenv("AZURE_COSMOS_DB", default="azimageai")
+)
+
+cosmos_container_client = cosmos_database_client.get_container_client(
+    getenv("AZURE_COSMOS_CONTAINER", default="images")
+)
+
+while True:
+
     print("Receiving messages...")
-    batches = queue_client.receive_messages(messages_per_page=os.getenv("AZURE_STORAGE_QUEUE_MSG_COUNT"))
+    batches = queue_client.receive_messages(
+        messages_per_page=getenv("AZURE_STORAGE_QUEUE_MSG_COUNT", default="10")
+    )
     for batch in batches.by_page():
         for message in batch:
             message_json = DotMap(json.loads(message.content))
@@ -40,12 +65,13 @@ while(True):
             fr_poller = fr_client.begin_recognize_content_from_url(message_json.url)
             fr_result = fr_poller.result()
 
-            text = ''
-
+            lines_of_text = []
             for page in fr_result:
                 for line in page.lines:
-                    text += line.text + ' '
-            
+                    lines_of_text.append(line.text)
+            text = ' '.join(lines_of_text)
+            lines_of_text.clear()
+
             print(text)
             message_json.text = text
 
@@ -60,4 +86,6 @@ while(True):
                 queue_client.delete_message(message)
 
             print(message_json)
-    time.sleep(1)
+
+    time.sleep(getenv("AZURE_STORAGE_QUEUE_RECEIVE_SLEEP", type=bool, default=1))
+
