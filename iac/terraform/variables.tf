08845af7a1823cd57417ff02bb3119d6cb286102
Jon Gallant <jon@jongallant.com>
2020-07-23 17:05:14 -0700

    Add aks support

--- iac/terraform/variables.tf
+++ iac/terraform/variables.tf
@@ -1,17 +1,17 @@
 variable "basename" {
   type        = string
   description = "The base name for all resources"
-  default     = "azsdkdemo99"
+  default     = "azsdkdemo100"
 }
 
 variable "location" {
   type        = string
   description = "Azure region where to create resources."
-  default     = "West US"
+  default     = "westus2"
 }
 
 variable "failover_location" {
   type        = string
   description = "Cosmos failover"
-  default     = "East US"
+  default     = "eastus2"
 }
