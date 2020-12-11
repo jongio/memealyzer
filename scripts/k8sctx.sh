486cc0f0fc752bb45e12ac9ba2ec8a51c8563dcb
Jon Gallant <2163001+jongio@users.noreply.github.com>
2020-12-11 15:33:47 -0800

    Update refs (#44)

--- scripts/k8sctx.sh
+++ scripts/k8sctx.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 echo "GETTING AKS CREDENTIALS"
-az aks get-credentials --resource-group ${BASENAME}rg --name ${BASENAME}aks
+az aks get-credentials --resource-group ${BASENAME}rg --name ${BASENAME}aks --overwrite-existing
 
 echo "SWITCHING K8S CONTEXT"
 kubectl config use-context ${K8S_CONTEXT}
