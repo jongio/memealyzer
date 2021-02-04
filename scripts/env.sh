75ac02ea0a07e71e0c29de40d865b377e6290dde
Jon Gallant <2163001+jongio@users.noreply.github.com>
2021-02-04 00:12:18 +0000

    Update envscript for codespaces

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -euo pipefail
 
-if [ "$ENV" = "local" ]
-then
+if [[ "${CLOUDENV_ENVIRONMENT_ID:-}" ]]; then
+    export API_ENDPOINT=https://${CLOUDENV_ENVIRONMENT_ID}-2080.apps.codespaces.githubusercontent.com
+    export FUNCTIONS_ENDPOINT=https://${CLOUDENV_ENVIRONMENT_ID}-3080.apps.codespaces.githubusercontent.com
+elif [ "$ENV" = "local" ]; then
     export FUNCTIONS_ENDPOINT=
 else
     export FUNCTIONS_ENDPOINT=http://${BASENAME}function.azurewebsites.net
