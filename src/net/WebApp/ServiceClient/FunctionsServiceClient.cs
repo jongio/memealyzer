5416eb13a2fbc54bd09364dabc69bfe97f2dd0e7
Jon Gallant <2163001+jongio@users.noreply.github.com>
2021-02-05 02:20:55 +0000

    Update fn code

--- src/net/WebApp/ServiceClient/FunctionsServiceClient.cs
+++ src/net/WebApp/ServiceClient/FunctionsServiceClient.cs
@@ -1,4 +1,8 @@
+using System;
 using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.WebAssembly.Http;
 using Microsoft.AspNetCore.SignalR.Client;
 
 public class FunctionsServiceClient : ServiceClient
@@ -10,8 +14,25 @@ public class FunctionsServiceClient : ServiceClient
     public HubConnection GetHubConnection()
     {
         return new HubConnectionBuilder()
-            .WithUrl(base.httpClient.BaseAddress + "api")
+            .WithUrl(base.httpClient.BaseAddress + "api", options =>
+            {
+                options.HttpMessageHandlerFactory = innerHandler =>
+                new IncludeRequestCredentialsMessageHandler { InnerHandler = innerHandler };
+            })
             .WithAutomaticReconnect()
             .Build();
+    }
+}
+
+
+public class IncludeRequestCredentialsMessageHandler : DelegatingHandler
+{
+    protected override Task<HttpResponseMessage> SendAsync(
+        HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        Console.WriteLine("Hit here");
+        Console.WriteLine(request.RequestUri);
+        request.SetBrowserRequestCredentials(BrowserRequestCredentials.Include);
+        return base.SendAsync(request, cancellationToken);
     }
 }
