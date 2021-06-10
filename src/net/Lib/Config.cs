3586ba75a61154d9174a9b0209bc5c17cbb7d6e2
Jon Gallant <jong@microsoft.com>
2021-06-10 11:53:58 -0700

    Refactor config

--- src/net/Lib/Config.cs
+++ src/net/Lib/Config.cs
@@ -13,7 +13,7 @@ namespace Lib
         {
             get
             {
-                return String.Format(Env.GetString("AZURE_COSMOS_ENDPOINT", UseCosmosEmulator ? "https://localhost:8081" : "https://{0}cosmosaccount.documents.azure.com:443"), BaseName);
+                return Env.GetString("AZURE_COSMOS_ENDPOINT", UseCosmosEmulator ? "https://localhost:8081" : $"https://{BaseName}cosmosaccount.documents.azure.com:443");
             }
         }
 
@@ -30,15 +30,15 @@ namespace Lib
         public static string CosmosDB { get { return Env.GetString("AZURE_COSMOS_DB", "memealyzer"); } }
         public static string CosmosCollection { get { return Env.GetString("AZURE_COSMOS_COLLECTION", "images"); } }
         public static string CosmosKeySecretName { get { return Env.GetString("AZURE_COSMOS_KEY_SECRET_NAME", "CosmosKey"); } }
-        public static Uri FormRecognizerEndpoint { get { return new Uri(String.Format(Env.GetString("AZURE_FORM_RECOGNIZER_ENDPOINT", "https://{0}fr.cognitiveservices.azure.com/"), BaseName)); } }
-        public static Uri KeyVaultEndpoint { get { return new Uri(String.Format(Env.GetString("AZURE_KEYVAULT_ENDPOINT", "https://{0}kv.vault.azure.net/"), BaseName)); } }
-        public static string StorageAccountName { get { return String.Format(Env.GetString("AZURE_STORAGE_ACCOUNT_NAME", UseAzurite ? "devstoreaccount1" : "{0}storage"), BaseName); } }
-        public static Uri StorageBlobEndpoint { get { return new Uri(String.Format(Env.GetString("AZURE_STORAGE_BLOB_ENDPOINT", UseAzurite ? "https://127.0.0.1:10000/devstoreaccount1" : "https://{0}storage.blob.core.windows.net/"), BaseName)); } }
-        public static Uri StorageQueueEndpoint { get { return new Uri(String.Format(Env.GetString("AZURE_STORAGE_QUEUE_ENDPOINT", UseAzurite ? "https://127.0.0.1:10001/devstoreaccount1" : "https://{0}storage.queue.core.windows.net/"), BaseName)); } }
-        public static Uri StorageTableEndpoint { get { return new Uri(String.Format(Env.GetString("AZURE_STORAGE_TABLE_ENDPOINT", UseAzurite ? "http://127.0.0.1:10002/devstoreaccount1" : "https://{0}storage.table.core.windows.net/"), BaseName)); } }
-        public static Uri TextAnalyticsEndpoint { get { return new Uri(String.Format(Env.GetString("AZURE_TEXT_ANALYTICS_ENDPOINT", "https://{0}ta.cognitiveservices.azure.com/"), BaseName)); } }
-        public static Uri AppConfigEndpoint { get { return new Uri(String.Format(Env.GetString("AZURE_APP_CONFIG_ENDPOINT", "https://{0}appconfig.azconfig.io"), BaseName)); } }
-        public static string ServiceBusNamespace { get { return String.Format(Env.GetString("AZURE_SERVICE_BUS_NAMESPACE", "{0}sb.servicebus.windows.net"), BaseName); } }
+        public static Uri FormRecognizerEndpoint { get { return new Uri(Env.GetString("AZURE_FORM_RECOGNIZER_ENDPOINT", $"https://{BaseName}fr.cognitiveservices.azure.com/")); } }
+        public static Uri KeyVaultEndpoint { get { return new Uri(Env.GetString("AZURE_KEYVAULT_ENDPOINT", $"https://{BaseName}kv.vault.azure.net/")); } }
+        public static string StorageAccountName { get { return Env.GetString("AZURE_STORAGE_ACCOUNT_NAME", UseAzurite ? "devstoreaccount1" : $"{BaseName}storage"); } }
+        public static Uri StorageBlobEndpoint { get { return new Uri(Env.GetString("AZURE_STORAGE_BLOB_ENDPOINT", UseAzurite ? "https://127.0.0.1:10000/devstoreaccount1" : $"https://{BaseName}storage.blob.core.windows.net/")); } }
+        public static Uri StorageQueueEndpoint { get { return new Uri(Env.GetString("AZURE_STORAGE_QUEUE_ENDPOINT", UseAzurite ? "https://127.0.0.1:10001/devstoreaccount1" : $"https://{BaseName}storage.queue.core.windows.net/")); } }
+        public static Uri StorageTableEndpoint { get { return new Uri(Env.GetString("AZURE_STORAGE_TABLE_ENDPOINT", UseAzurite ? "http://127.0.0.1:10002/devstoreaccount1" : $"https://{BaseName}storage.table.core.windows.net/")); } }
+        public static Uri TextAnalyticsEndpoint { get { return new Uri(Env.GetString("AZURE_TEXT_ANALYTICS_ENDPOINT", $"https://{BaseName}ta.cognitiveservices.azure.com/")); } }
+        public static Uri AppConfigEndpoint { get { return new Uri(Env.GetString("AZURE_APP_CONFIG_ENDPOINT", $"https://{BaseName}appconfig.azconfig.io")); } }
+        public static string ServiceBusNamespace { get { return Env.GetString("AZURE_SERVICE_BUS_NAMESPACE", $"{BaseName}sb.servicebus.windows.net"); } }
         public static string StorageBlobContainerName { get { return Env.GetString("AZURE_STORAGE_BLOB_CONTAINER_NAME", "blobs"); } }
         public static string MessagesQueueName { get { return Env.GetString("AZURE_MESSAGES_QUEUE_NAME", "messages"); } }
         public static int StorageQueueMessageCount { get { return Env.GetInt("AZURE_STORAGE_QUEUE_MSG_COUNT", 10); } }
