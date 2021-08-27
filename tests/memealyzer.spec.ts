f7f9713a9e8e22745f7d56352f563d95cdef689d
Jon Gallant <jon@jongallant.com>
2021-08-27 21:31:58 +0000

    add waitForResponse

--- tests/memealyzer.spec.ts
+++ tests/memealyzer.spec.ts
@@ -1,24 +1,27 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-var uri:string = 'http://localhost:1080/'
+var uri: string = "http://localhost:1080/";
 
-test('Add Meme Test', async ({ page }) => {
-  
+test("Add Meme Test", async ({ page }) => {
   await page.goto(uri);
 
-  const add = page.locator('.form-row button').first();
-  
-  await page.waitForSelector('.spinner-grow', {state: 'detached'});
+  const add = page.locator(".form-row button").first();
 
-  const cards = page.locator('.card');
+  await page.waitForSelector(".spinner-grow", { state: "detached" });
+
+  const cards = page.locator(".card");
 
   const beforeCardCount = await cards.count();
 
-  await add.click()
-  
-  await page.waitForTimeout(1000);
+  await Promise.all([
+    page.waitForResponse(
+      (response) =>
+        response.url().endsWith("/image") && response.status() === 200
+    ),
+    await add.click(),
+  ]);
 
   const afterCardCount = await cards.count();
-  
+
   expect(afterCardCount).toEqual(beforeCardCount + 1);
 });
