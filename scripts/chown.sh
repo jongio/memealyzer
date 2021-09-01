a72bd301a6eb87a72f0590bc22251fe540ca18a7
Jon Gallant <jon@jongallant.com>
2021-09-01 17:28:47 +0000

    Fix chown path

--- scripts/chown.sh
+++ scripts/chown.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 
 # You may need to run this in your devcontainer if you get build errors.
-sudo chown -R codespace ~/workspace/src
+sudo chown -R codespace /home/codespace/workspace/src
